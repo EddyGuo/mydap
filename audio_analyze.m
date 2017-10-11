a2ceb055b4cf340eeb1150feafd630958a838e92
eddyguo <eddygcn@163.com>
2017-10-12 00:22:15 +0800

    change yaxis to power

--- audio_analyze.m
+++ audio_analyze.m
@@ -10,23 +10,25 @@ switch wavetype
     case 2
         %幅频曲线
         fft_sample=fft(handles.Sample,nfft);
-        y=abs(fft_sample/nfft);%矫正fft幅值
+        y=fft_sample.*conj(fft_sample)/nfft;%Parseval定理求功率
         %由于fft得到共轭对称的两部分分量，幅值为时域的一半（除了0处直流分量）
         y0=fftshift(y);%循环移位，取中间为0
         %The fftshift function rearranges the output from fft with a circular shift to produce a 0-centered periodogram.
         f0=(-nfft/2:nfft/2-1)*(handles.Fs/nfft);
         plot(handles.axes1,f0,y0,'r');
         xlabel(handles.axes1,'频率(Hz)');
+        ylabel(handles.axes1,'功率');
     case 3
         %幅频曲线(DB)
         fft_sample=fft(handles.Sample,nfft);
-        y=abs(fft_sample/nfft);
-        y1=20*log10(y);
+        y=fft_sample.*conj(fft_sample)/nfft;
+        y1=10*log10(y);
         y2=y1(1:nfft/2+1);%取频谱一半
         y2(2:end-1)=2*y2(2:end-1);%直流分量保持不变,其他乘2
         f=handles.Fs*(0:(nfft/2))/nfft;
         plot(handles.axes1,f,y2,'y');
         xlabel(handles.axes1,'频率(Hz)');
+        ylabel(handles.axes1,'功率(DB)');
     case 4
         %相频曲线
         fft_sample=fft(handles.Sample,nfft);
@@ -39,7 +41,7 @@ switch wavetype
         %瀑布频谱图
         axes(handles.axes1);
         hammingwin=2^floor(log2(sample_length))/8;
-        spectrogram(handles.Sample,hammingwin,hammingwin/2,sample_length,handles.Fs);
+        spectrogram(handles.Sample,hammingwin,hammingwin/2,nfft,handles.Fs);
         set(colorbar,'Color','[0.5,0.5,0.5]');
     case 6
         %音压曲线
