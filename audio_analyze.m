d7db2d23d69f0ee86e6af6def8252aa384c094db
eddyguo <eddygcn@163.com>
2017-10-15 19:53:16 +0800

    optimize uigetfile

--- audio_analyze.m
+++ audio_analyze.m
@@ -1,54 +1,57 @@
-function audio_analyze(wavetype,handles)
+function audio_analyze(handles)
 % --- plot wave
-sample_length=length(handles.Sample);
-t=(0:sample_length-1)/handles.Fs;
-nfft=pow2(nextpow2(sample_length));% fft长度，蝶形算法取2的幂次方提高速度
-switch wavetype
-    case 1
-        % --- 时域波形
-        plot(handles.axes1,t,handles.Sample,'g');
-        xlabel(handles.axes1,'时间(s)');
-    case 2
-        % --- 频率响应曲线
-        fft_sample=fft(handles.Sample,nfft);
-        y=abs(fft_sample)/nfft;
-        % 由于fft得到共轭对称的两部分分量，幅值为时域的一半（除了0处直流分量）
-        y0=fftshift(y);% 循环移位，取中间为0
-        f0=(-nfft/2:nfft/2-1)*(handles.Fs/nfft);
-        plot(handles.axes1,f0,y0,'r');
-        xlabel(handles.axes1,'频率(Hz)');
-        ylabel(handles.axes1,'幅值');
-    case 4
-        % --- 能量谱
-        fft_sample=fft(handles.Sample,nfft);
-        y=abs(fft_sample).^2/nfft;% Parseval定理求能量
-        y1=10*log10(y);
-        y2=y1(1:nfft/2+1);% 取频谱一半
-        y2(2:end-1)=2*y2(2:end-1);% 直流分量保持不变,其他乘2
-        f=handles.Fs*(0:(nfft/2))/nfft;
-        plot(handles.axes1,f,y2,'y');
-        xlabel(handles.axes1,'频率(Hz)');
-        ylabel(handles.axes1,'谱密度(DB/Hz)');
-    case 3
-        % --- 相频曲线
-        fft_sample=fft(handles.Sample,nfft);
-        f0=(-nfft/2:nfft/2-1)*(handles.Fs/nfft);
-        ph_y0=fftshift(fft_sample);
-        phase=unwrap(angle(ph_y0));% 矫正相角跳变范围在pi以内
-        plot(handles.axes1,f0,phase,'b');
-        xlabel(handles.axes1,'频率(Hz)');
-    case 5
-        % --- 瀑布频谱图
-        axes(handles.axes1);
-        hammingwin=2^floor(log2(sample_length))/8;
-        spectrogram(handles.Sample,hammingwin,hammingwin/2,nfft,handles.Fs);
-        colormap(hot);
-    case 6
-        % --- 音压曲线
-        audio_db=20*log10(abs(handles.Sample));% 求音压
-        plot(handles.axes1,t,audio_db,'c');
-        xlabel(handles.axes1,'时间(s)');
-        ylabel(handles.axes1,'音压(DB)');
+wavetype=get(handles.wave_select_listbox,'value');
+if ~isempty(handles.Sample)
+    sample_length=length(handles.Sample);
+    t=(0:sample_length-1)/handles.Fs;
+    nfft=pow2(nextpow2(sample_length));% fft长度，蝶形算法取2的幂次方提高速度
+    switch wavetype
+        case 1
+            % --- 时域波形
+            plot(handles.axes1,t,handles.Sample,'g');
+            xlabel(handles.axes1,'时间(s)');
+        case 2
+            % --- 频率响应曲线
+            fft_sample=fft(handles.Sample,nfft);
+            y=abs(fft_sample)/nfft;
+            % 由于fft得到共轭对称的两部分分量，幅值为时域的一半（除了0处直流分量）
+            y0=fftshift(y);% 循环移位，取中间为0
+            f0=(-nfft/2:nfft/2-1)*(handles.Fs/nfft);
+            plot(handles.axes1,f0,y0,'r');
+            xlabel(handles.axes1,'频率(Hz)');
+            ylabel(handles.axes1,'幅值');
+        case 4
+            % --- 能量谱
+            fft_sample=fft(handles.Sample,nfft);
+            y=abs(fft_sample).^2/nfft;% Parseval定理求能量
+            y1=10*log10(y);
+            y2=y1(1:nfft/2+1);% 取频谱一半
+            y2(2:end-1)=2*y2(2:end-1);% 直流分量保持不变,其他乘2
+            f=handles.Fs*(0:(nfft/2))/nfft;
+            plot(handles.axes1,f,y2,'y');
+            xlabel(handles.axes1,'频率(Hz)');
+            ylabel(handles.axes1,'谱密度(DB/Hz)');
+        case 3
+            % --- 相频曲线
+            fft_sample=fft(handles.Sample,nfft);
+            f0=(-nfft/2:nfft/2-1)*(handles.Fs/nfft);
+            ph_y0=fftshift(fft_sample);
+            phase=unwrap(angle(ph_y0));% 矫正相角跳变范围在pi以内
+            plot(handles.axes1,f0,phase,'b');
+            xlabel(handles.axes1,'频率(Hz)');
+        case 5
+            % --- 瀑布频谱图
+            axes(handles.axes1);
+            hammingwin=2^floor(log2(sample_length))/8;
+            spectrogram(handles.Sample,hammingwin,hammingwin/2,nfft,handles.Fs);
+            colormap(hot);
+        case 6
+            % --- 音压曲线
+            audio_db=20*log10(abs(handles.Sample));% 求音压
+            plot(handles.axes1,t,audio_db,'c');
+            xlabel(handles.axes1,'时间(s)');
+            ylabel(handles.axes1,'音压(DB)');
+    end
+    grid on;
 end
-grid on;
     
--- dsp.m
+++ dsp.m
@@ -1,11 +1,11 @@
 function varargout = dsp(varargin)
 gui_Singleton = 1;
 gui_State = struct('gui_Name',       mfilename, ...
-                   'gui_Singleton',  gui_Singleton, ...
-                   'gui_OpeningFcn', @dsp_OpeningFcn, ...
-                   'gui_OutputFcn',  @dsp_OutputFcn, ...
-                   'gui_LayoutFcn',  [] , ...
-                   'gui_Callback',   []);
+    'gui_Singleton',  gui_Singleton, ...
+    'gui_OpeningFcn', @dsp_OpeningFcn, ...
+    'gui_OutputFcn',  @dsp_OutputFcn, ...
+    'gui_LayoutFcn',  [] , ...
+    'gui_Callback',   []);
 if nargin && ischar(varargin{1})
     gui_State.gui_Callback = str2func(varargin{1});
 end
@@ -37,7 +37,7 @@ handles.index=0;
 guidata(hObject, handles);
 
 % --- Outputs from this function are returned to the command line.
-function varargout = dsp_OutputFcn(hObject, eventdata, handles) 
+function varargout = dsp_OutputFcn(hObject, eventdata, handles)
 
 % Get default command line output from handles structure
 varargout{1} = handles.output;
@@ -64,9 +64,12 @@ end
 
 % --- 文件输入音频
 function file_choose_pushbutton_Callback(hObject, eventdata, handles)
-[filename,pathname]=uigetfile({'*.wav;*.mp3;*.flac','音频文件(*.wav,*.mp3,*.flac)'},'选择文件');%弹出选择文件窗口
+[filename,pathname]=uigetfile({'*.wav;*.mp3;*.flac', ...
+    '音频文件(*.wav,*.mp3,*.flac)'},'选择文件');%弹出选择文件窗口
 % 判断文件为空
-if isempty(filename)||isempty(pathname)
+% 不能使用if isempty(filename)||isempty(pathname)
+% 取消窗口时会报错，取消时uigetfile返回filename为0
+if filename==0
     return
 else
     handles.Filepath=[pathname,filename];
@@ -78,9 +81,11 @@ else
         handles.Sample=handles.Sample(:,1);
     end
     handles.player=audioplayer(handles.Sample,handles.Fs);
-    setplayer(eventdata,handles);
+    setplayer(handles);
+    
+    guidata(hObject,handles);
 end
- guidata(hObject,handles);
+
 
 % --- 录音按钮
 function record_start_pushbutton_Callback(hObject, eventdata, handles)
@@ -91,10 +96,11 @@ fs=str2double(fs_list{fs_value});% 获取选定采样率
 handles.Fs=fs;
 
 handles.recObj=audiorecorder(fs,16,1);% 创建一个录音器
-record(handles.recObj);% 开始录音
 
-set(hObject,'enable','off');
-set(handles.record_stop_pushbutton,'enable','on');
+set(handles.recObj,'StartFcn',{@recordstart_Callback,handles}, ...
+    'StopFcn',{@recordstop_Callback,handles}); % 录音回调
+
+record(handles.recObj);% 开始录音
 
 guidata(hObject,handles);
 
@@ -104,22 +110,17 @@ stop(handles.recObj);% 停止录音
 handles.Sample=getaudiodata(handles.recObj);% 获取录音
 handles.index=handles.index+1;
 handles.player=audioplayer(handles.Sample,handles.Fs);
-setplayer(eventdata,handles);
-
-set(handles.play_pushbutton,'enable','on');
-set(handles.play_stop_pushbutton,'enable','on');
-set(handles.record_start_pushbutton,'enable','on');
-set(hObject,'enable','off');
+setplayer(handles);
 
 guidata(hObject,handles);
 
 % --- 播放器设置
-function setplayer(eventdata,handles)
+function setplayer(handles)
 % 创建player回调函数
 set(handles.player,'StartFcn',{@playstart_Callback,handles}, ...
-        'StopFcn',{@playstop_Callback,handles});
+    'StopFcn',{@playstop_Callback,handles});
 % plot wave
-feval(@wave_select_listbox_Callback,handles.wave_select_listbox,eventdata,handles);
+audio_analyze(handles);
 
 % --- 播放按钮
 function play_pushbutton_Callback(hObject, eventdata, handles)
@@ -128,7 +129,6 @@ play(handles.player);% 开始播放
 % --- 停止播放按钮
 function play_stop_pushbutton_Callback(hObject, eventdata, handles)
 stop(handles.player);% 停止播放
-set(handles.play_pushbutton,'enable','on');
 
 % --- 输入方式按钮组
 function uibuttongroup1_SelectionChangedFcn(hObject, eventdata, handles)
@@ -153,23 +153,36 @@ end
 
 % --- 波形选择栏
 function wave_select_listbox_Callback(hObject, eventdata, handles)
-wavetype=get(hObject,'value');
-if ~isempty(handles.Sample)
-    audio_analyze(wavetype,handles);
-end
+audio_analyze(handles);
 
 % --- Executes during object creation, after setting all properties.
 function wave_select_listbox_CreateFcn(hObject, eventdata, handles)
 if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgroundColor'))
     set(hObject,'BackgroundColor','white');
-end
+end 
 
 % --- 播放开始
 function playstart_Callback(hObject,eventdata,handles)
 set(handles.play_pushbutton,'enable','off');
 set(handles.play_stop_pushbutton,'enable','on');
+set(handles.playstate_text,'String','状态栏> 正在播放...');
 
 % --- 播放结束
 function playstop_Callback(hObject,eventdata,handles)
 set(handles.play_pushbutton,'enable','on');
 set(handles.play_stop_pushbutton,'enable','off');
+set(handles.playstate_text,'String','状态栏>');
+
+% --- 录音开始
+function recordstart_Callback(hObject,eventdata,handles)
+set(handles.record_start_pushbutton,'enable','off');
+set(handles.record_stop_pushbutton,'enable','on');
+set(handles.playstate_text,'String','状态栏> 正在录音...');
+
+% --- 录音结束
+function recordstop_Callback(hObject,eventdata,handles)
+set(handles.play_pushbutton,'enable','on');
+set(handles.play_stop_pushbutton,'enable','on');
+set(handles.record_start_pushbutton,'enable','on');
+set(handles.record_stop_pushbutton,'enable','off');
+set(handles.playstate_text,'String','状态栏>');
